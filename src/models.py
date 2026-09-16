e3cf8709ff034e264ce7e40eb875b479dc49af17
Tracy <ngda-sil@student.42lausanne.ch>
2026-09-16 14:29:41 +0200

    Add clearer error messages for invalid input

--- src/models.py
+++ src/models.py
@@ -20,17 +20,19 @@ class IntervalSession:
     effort_duration_ms: int = field(init=False)
 
     def __post_init__(self):
+        if self.dist_m <= 0:
+            raise ValueError("distance must be greater than 0")
+        if self.m_pace < 0:
+            raise ValueError("pace minutes must be greater than 0")
+        if (self.s_pace < 0) or (self.s_pace >= 60):
+            raise ValueError("pace seconds must be between 0 and 59")
+        if self.m_pace + self.s_pace == 0:
+            raise ValueError("pace cannot be 0:00")
+        if self.rest_s < 0:
+            raise ValueError("rest seconds must be 0 or greater")
+        if self.rep <= 0:
+            raise ValueError("repetitions must be greater than 0")
 
-        if (
-            (self.dist_m <= 0)
-            or (self.m_pace < 0)
-            or (self.s_pace < 0)
-            or (self.s_pace >= 60)
-            or (self.m_pace + self.s_pace == 0)
-            or (self.rest_s < 0)
-            or (self.rep <= 0)
-        ):
-            raise ValueError("Invalid user input")
         self.rest_ms = s_to_ms(self.rest_s)
         self.effort_duration_ms = self._calculate_effort_duration_in_ms(
             self.m_pace, self.s_pace, self.dist_m
